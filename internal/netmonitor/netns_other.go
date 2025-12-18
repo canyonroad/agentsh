945ec46191c8f7f6336d0b0a040846c9d20d81d2
Eran Sandler <eran@sandler.co.il>
2025-12-18 10:34:31 -0800

    Fix non-Linux netns stub for builds

--- internal/netmonitor/netns_other.go
+++ internal/netmonitor/netns_other.go
@@ -7,11 +7,19 @@ import (
 	"fmt"
 )
 
-type NetNS struct{}
+type NetNS struct {
+	Name         string
+	HostIfName   string
+	NSIfName     string
+	HostIP       string
+	NSIP         string
+	SubnetCIDR   string
+	ProxyTCPPort int
+	DNSUDPPort   int
+}
 
 func SetupNetNS(ctx context.Context, nsName string, subnetCIDR string, hostIf string, nsIf string, hostIPCIDR string, nsIPCIDR string, proxyTCPPort int, dnsUDPPort int) (*NetNS, error) {
 	return nil, fmt.Errorf("transparent netns not supported on this platform")
 }
 
 func (n *NetNS) Close(ctx context.Context) error { return nil }
-
