9de513ad6a892177619674ee0519121488adb579
Eran Sandler <eran@sandler.co.il>
2025-12-19 08:16:15 -0800

    Fix non-Linux build: add MapCounts stub

--- internal/netmonitor/ebpf/maps_other.go
+++ internal/netmonitor/ebpf/maps_other.go
@@ -4,6 +4,17 @@ package ebpf
 
 import "fmt"
 
+type MapCounts struct {
+	AllowCgroup    uint64
+	AllowTotal     uint64
+	DenyCgroup     uint64
+	DenyTotal      uint64
+	LpmAllowCgroup uint64
+	LpmAllowTotal  uint64
+	LpmDenyCgroup  uint64
+	LpmDenyTotal   uint64
+}
+
 func PopulateAllowlist(_ any, _ uint64, _ []AllowKey, _ []AllowCIDR, _ []AllowKey, _ []AllowCIDR, _ bool) error {
 	return fmt.Errorf("ebpf maps not supported on this platform")
 }
